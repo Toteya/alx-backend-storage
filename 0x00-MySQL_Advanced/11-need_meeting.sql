23cace5dee8174628d5fc2c9d8c6f8b9959a179a
Toteya <111172685+Toteya@users.noreply.github.com>
2024-07-11 19:04:39 +0200

    Correction: date comparison

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,8 +1,9 @@
 -- Creates a view that lists students that have a score under 80 and no last
 -- meeting or last meeting longer than a month ago
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
 WHERE score < 80
-AND (last_meeting > ADDDATE(CURDATE(), INTERVAL -1 MONTH)
+AND (last_meeting < ADDDATE(CURDATE(), INTERVAL -1 MONTH)
 OR last_meeting IS NULL);
