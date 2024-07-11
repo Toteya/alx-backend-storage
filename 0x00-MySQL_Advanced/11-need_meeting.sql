be4a86bd06294f46f5e75bcb09ed4c719539bcd3
Toteya <111172685+Toteya@users.noreply.github.com>
2024-07-11 18:58:27 +0200

    Create a view

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- Creates a view that lists students that have a score under 80 and no last
+-- meeting or last meeting longer than a month ago
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+AND (last_meeting > ADDDATE(CURDATE(), INTERVAL -1 MONTH)
+OR last_meeting IS NULL);
