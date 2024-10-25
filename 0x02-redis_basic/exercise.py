fbad42a3898f2f392d146f6369b21ed1c0f336e6
Toteya <111172685+Toteya@users.noreply.github.com>
2024-10-25 18:21:46 +0200

    correction: flushdb, annotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,11 +16,29 @@ class Cache:
     def __init__(self):
         """initialise cache instance"""
         self._redis = redis.Redis()
+        self._redis.flushdb()
 
-    def store(self, data: Union[bytes, str, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Takes an argument and returns a string
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn=None):
+        """
+        Returns data corresponding to the given key
+        """
+        data = self._redis.get(key)
+        if data and fn:
+            return fn(key)
+        else:
+            return data
+    
+    def get_str(self, ):
+        """
+        Parametrises Cache.get
+        """
+        
+
